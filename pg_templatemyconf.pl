7f3ce1803709457ec771d899665af01d16038b61
Francisco J. Tsao Santín <tsao@disroot.org>
2022-04-29 23:17:51 +0200

    Fix indentation

--- pg_templatemyconf.pl
+++ pg_templatemyconf.pl
@@ -17,62 +17,61 @@ my $template_file = $args->{'-t'};
 my $defaults_file = $args->{'-d'};
 
 if ( ! open CONFIG, '<', $config_file ) {
-die "Cannot open config file: $!";
+	die "Cannot open config file: $!";
 }
 if ( ! open TEMPLATE, '>', $template_file ) {
-die "Cannot create template file: $!";
+	die "Cannot create template file: $!";
 }
 if ( ! open DEFAULTS, '>', $defaults_file ) {
-die "Cannot create defaults file: $!";
+	die "Cannot create defaults file: $!";
 }
 
 print TEMPLATE "{{ ansible_managed | comment }}\n";
 print DEFAULTS "---\n\n";
 while (<CONFIG>) {
-    if (/^(\w+) = (-?\d+\.?\d*)(\s*)\#(.*)$/) {
-    print TEMPLATE "$1 = {{ pg_$1 | default($2) }}$3\#$4\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^(\w+) = (\w+)(\s*)\#(.*)$/) {
-    print TEMPLATE "$1 = {{ pg_$1 | default('$2') }}$3\#$4\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^(\w+) = (\'\S*\')(\s*)\#(.*)$/) {
-    print TEMPLATE "$1 = '{{ pg_$1 | default($2) }}'$3\#$4\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^(\w+) = (-?\d+\.?\d*)$/) {
-    print TEMPLATE "$1 = {{ pg_$1 | default($2) }}\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^(\w+) = (\w+)$/) {
-    print TEMPLATE "$1 = {{ pg_$1 | default('$2') }}\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^(\w+) = (\'\S*\')$/) {
-    print TEMPLATE "$1 = '{{ pg_$1 | default($2) }}'\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^\#(\w+) = (-?\d+\.?\d*)(\s+)(.*)$/) {
-    print TEMPLATE;
-    print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = {{ pg_$1 | default($2) }}\n{% endif %}\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^\#(\w+) = (\w+)(\s+)(.*)$/) {
-    print TEMPLATE;
-    print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = {{ pg_$1 | default('$2') }}\n{% endif %}\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^\#(\w+) = (\'\S*\')(\s+)(.*)$/) {
-    print TEMPLATE;
-    print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = '{{ pg_$1 | default($2) }}'\n{% endif %}\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } elsif (/^\#(\w+) = (-?\d+\.?\d*)$/) {
-    print TEMPLATE;
-    print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = {{ pg_$1 | default($2) }}\n{% endif %}\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    }  elsif (/^\#(\w+) = (\w+)$/) {
-    print TEMPLATE;
-    print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = {{ pg_$1 | default('$2') }}\n{% endif %}\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    }  elsif (/^\#(\w+) = (\'\S*\')$/) {
-    print TEMPLATE;
-    print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = '{{ pg_$1 | default($2) }}'\n{% endif %}\n";
-	print DEFAULTS "\# pg_$1: $2\n";
-    } else {
-    print TEMPLATE;
-    }
-
+	if (/^(\w+) = (-?\d+\.?\d*)(\s*)\#(.*)$/) {
+		print TEMPLATE "$1 = {{ pg_$1 | default($2) }}$3\#$4\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^(\w+) = (\w+)(\s*)\#(.*)$/) {
+		print TEMPLATE "$1 = {{ pg_$1 | default('$2') }}$3\#$4\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^(\w+) = (\'\S*\')(\s*)\#(.*)$/) {
+		print TEMPLATE "$1 = '{{ pg_$1 | default($2) }}'$3\#$4\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^(\w+) = (-?\d+\.?\d*)$/) {
+		print TEMPLATE "$1 = {{ pg_$1 | default($2) }}\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^(\w+) = (\w+)$/) {
+		print TEMPLATE "$1 = {{ pg_$1 | default('$2') }}\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^(\w+) = (\'\S*\')$/) {
+		print TEMPLATE "$1 = '{{ pg_$1 | default($2) }}'\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^\#(\w+) = (-?\d+\.?\d*)(\s+)(.*)$/) {
+		print TEMPLATE;
+		print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = {{ pg_$1 | default($2) }}\n{% endif %}\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^\#(\w+) = (\w+)(\s+)(.*)$/) {
+		print TEMPLATE;
+		print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = {{ pg_$1 | default('$2') }}\n{% endif %}\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^\#(\w+) = (\'\S*\')(\s+)(.*)$/) {
+		print TEMPLATE;
+		print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = '{{ pg_$1 | default($2) }}'\n{% endif %}\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} elsif (/^\#(\w+) = (-?\d+\.?\d*)$/) {
+		print TEMPLATE;
+		print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = {{ pg_$1 | default($2) }}\n{% endif %}\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	}  elsif (/^\#(\w+) = (\w+)$/) {
+		print TEMPLATE;
+		print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = {{ pg_$1 | default('$2') }}\n{% endif %}\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+    	}  elsif (/^\#(\w+) = (\'\S*\')$/) {
+		print TEMPLATE;
+		print TEMPLATE "{% if ( pg_$1 is defined ) %}\n$1 = '{{ pg_$1 | default($2) }}'\n{% endif %}\n";
+		print DEFAULTS "\# pg_$1: $2\n";
+	} else {
+		print TEMPLATE;
+	}
 }
